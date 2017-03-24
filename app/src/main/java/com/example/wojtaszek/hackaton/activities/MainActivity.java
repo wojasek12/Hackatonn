ec6e74114540053fb5c603a366443631cfc5111b
eminems95 <mrcn.mucha@gmail.com>
2017-03-25 00:55:51 +0100

    Firebase configuration

--- app/src/main/java/com/example/wojtaszek/hackaton/activities/MainActivity.java
+++ app/src/main/java/com/example/wojtaszek/hackaton/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.wojtaszek.hackaton;
+package com.example.wojtaszek.hackaton.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.wojtaszek.hackaton.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 }
